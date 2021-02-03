da09e238f8f8efe70c5ae8358cc17d2bb15fbb75
Maciej Gwizdz <maciej.gwizdz1994@gmail.com>
2021-02-03 16:43:12 +0100

    add basic routs and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,36 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'src/environments/environment';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { MaterialModule } from './shared/material/material.module';
+import { RouterModule } from '@angular/router';
+import { AdminOrdersComponent } from './admin/components/admin-orders/admin-orders.component';
+import { AdminBooksComponent } from './admin/components/admin-books/admin-books.component';
+import { ShoppingCartComponent } from './shopping/components/shopping-cart/shopping-cart.component';
+import { OrdersComponent } from './shopping/components/orders/orders.component';
+import { ProductsComponent } from './shopping/components/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AdminOrdersComponent,
+    AdminBooksComponent,
+    ShoppingCartComponent,
+    OrdersComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    MaterialModule,    
+    MaterialModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductsComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: 'orders', component: OrdersComponent },
+      { path: 'admin/orders', component: AdminOrdersComponent },
+      { path: 'admin/books', component: AdminBooksComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
