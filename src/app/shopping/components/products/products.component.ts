c54ee2de2c8541080c5481c3f14e886c532d7cac
Maciej Gwizdz <maciej.gwizdz1994@gmail.com>
2021-02-05 13:37:51 +0100

    implement product component

--- src/app/shopping/components/products/products.component.ts
+++ src/app/shopping/components/products/products.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Book } from 'src/app/shared/models/book';
+import { BookService } from 'src/app/shared/services/book.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  allBooks: Book[] = [];
+  filteredBooks: Book[] = [];
+  booksSubscribtion: Subscription;
+  categories = [
+    'All categories',
+    'Adventure',
+    'Fantasy',
+    'History'
+  ]
 
-  constructor() { }
+  constructor(private bookService: BookService) {
+    this.booksSubscribtion = bookService.books$.subscribe(books => {
+      this.allBooks = this.filteredBooks = books;
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    this.booksSubscribtion.unsubscribe();
+  }
+
+  filterBooksByCategory($event: any) {
+    if($event.value == 'All categories') {
+      this.filteredBooks = this.allBooks;  
+    } else {
+      this.filteredBooks = this.allBooks.filter(b => b.category == $event.value);
+    }    
+  }
 }
